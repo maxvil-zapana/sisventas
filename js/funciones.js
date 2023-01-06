e775073e508665f98cd2c96689b23f2df2694685
Maxvil Zapana <mzapana@cajalosandes.pe>
2023-01-06 18:50:57 -0500

    se esta realizando pruebas de ajax

--- js/funciones.js
+++ js/funciones.js
@@ -1,12 +1,34 @@
-function validarFormVacio(formulario){
-    datos=$('#' + formulario).serialize();
-    d=datos.split('&');
-    vacios=0;
-    for(i=0;i< d.length;i++){
-            controles=d[i].split("=");
-            if(controles[1]=="A" || controles[1]==""){
-                vacios++;
-            }
+function validarFormVacio(formulario) {
+  datos = $("#" + formulario).serialize();
+  d = datos.split("&");
+  vacios = 0;
+  for (i = 0; i < d.length; i++) {
+    controles = d[i].split("=");
+    if (controles[1] == "A" || controles[1] == "") {
+      vacios++;
     }
-    return vacios;
+  }
+  return vacios;
+}
+
+class Ajax {
+  constructor(objeto) {
+    this.tipo = objeto.metodo.toLowerCase();
+    this.url = objeto.url;
+    this.async = objeto.async;
+    this.respuesta = function () {
+      const data = "";
+      const peticion = new XMLHttpRequest();
+
+      peticion.open(this.tipo, this.url, this.async);
+      const valor = peticion.addEventListener("load", (e) => {
+    return e.target.response;
+        // console.log()
+      });
+
+      peticion.send();
+
+      return valor;
+   }
+  }
 }
